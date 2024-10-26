72233643d101a476ec89b4263c3f2dba6eabbaea
Ahmed Araby <ahmedaraby605@gmail.com>
2024-10-26 12:51:25 +0300

    provide routing based on the action method in addition to the request target (PATH/URL)

--- src/main/java/utils/TargetSwitch.java
+++ src/main/java/utils/TargetSwitch.java
@@ -8,6 +8,19 @@ import java.util.regex.Pattern;
 public class TargetSwitch {
 
     public static EndPoint getEndpoint(Request request) {
+
+        switch (request.method) {
+            case "GET":
+                return getSwitch(request);
+            case "POST":
+                return postSwitch(request);
+            default:
+                return new NotFoundEndpoint(); // we should indicate that the action method is not supported
+        }
+
+    }
+
+    public static EndPoint getSwitch(Request request) {
         final String[] tokens = Pattern.compile("/").split(request.target);
         if (tokens.length == 0) {
             return new IndexEndpoint(); // return index page
@@ -19,6 +32,14 @@ public class TargetSwitch {
                 return new UserAgentEndpoint();
             case "files":
                 return new FilesEndpoint();
+            default:
+                return new NotFoundEndpoint();
+        }
+    }
+
+    public static EndPoint postSwitch(Request request) {
+        final String[] tokens = Pattern.compile("/").split(request.target);
+        switch (tokens[1]) {
             default:
                 return new NotFoundEndpoint();
         }
